57556812454b942419a168a70cfb7e4439b3a562
A-Kija <42836152+A-Kija@users.noreply.github.com>
2019-05-30 19:51:25 +0300

    aa

--- f1.php
+++ f1.php
@@ -1,24 +1,31 @@
+<?php session_start() ?>
 Sugeneruotas skaicius:
 <?php
 
 if(!empty($_GET['page'])){
 
 if ($_GET['page'] == 1) {
-    echo rand(1,50);
+    echo $sk = rand(1,50);
     echo 'Pirmas';
 }
 elseif ($_GET['page'] == 2) {
-    echo rand(50,100);
+    echo $sk = rand(50,100);
     echo 'Antras';
 }
 elseif ($_GET['page'] == 3) {
-    echo rand(101,150);
+    echo $sk = rand(101,150);
     echo 'Trecias';
 }
 }
 
 ?>
 <br>
+Pries tai buves skaicius: 
+<?php
+echo $_SESSION['mano_sk'] ?? 'dar nera';
+
+$_SESSION['mano_sk'] = $sk; 
+?>
 <a href="?page=1">Pirmas</a><br>
 <a href="?page=2">Antras</a><br>
 <a href="?page=3">Trecias</a><br>
